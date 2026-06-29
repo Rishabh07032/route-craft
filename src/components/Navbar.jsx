6876c23394e1cd3a5f5c2594fa11c9f7abb684f1
Rishabh07032 <rishabhsinghjjhy77@gmail.com>
2026-06-29 23:48:03 +0530

    responsive 1.2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
